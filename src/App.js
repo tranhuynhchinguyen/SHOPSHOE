cceb5be0f1ffd4c5c368f3e2b8e6f6a72144b971
tranhuynhchinguyen <23110136@student.hcmute.edu.vn>
2024-07-23 09:51:23 +0700

    vendor: initilize shop shoe project study

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./global.scss";
+import { Button } from "antd";
+import HomePage from "./pages/HomePage";
+import Header from './components/shared/Header'
+import Footer from './components/shared/Footer'
+function App() {
+  return (
+   <BrowserRouter>
+   </BrowserRouter>
+  );
+}
+
+export default App;
+//35
---
+++ src/pages/ShoeDetailPage.jsx
@@ -0,0 +1,6 @@
+const ShoeDetailPage = () => {
+    return(
+        <div>Shoe Detail</div>
+    )
+}
+export default ShoeDetailPage;
